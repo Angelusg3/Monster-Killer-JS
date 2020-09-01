6a27b2e6bd5eb4cb2e5f4e08e10563175a8452f8
Naty T <nathan11002@gmail.com>
2020-09-01 16:57:53 +0300

    for of

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -187,11 +187,17 @@ function healPlayerHandler() {
 }
 
 function printLogHandler() {
-  for (let i = 0; i < battleLog.length; i++) {
-    console.log('-------');
-    console.log(battleLog[i]);
+  // for (let i = 0; i < battleLog.length; i++) {
+  //   console.log('-------');
+  //   console.log(battleLog[i]);
+  // }
+
+  for(const logEntry of battleLog){
+    console.log(logEntry);
   }
+
 }
+
 
 attackBtn.addEventListener('click', attackHandler);
 strongAttackBtn.addEventListener('click', strongAttackHandler);

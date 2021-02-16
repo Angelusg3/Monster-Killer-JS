4c4fa848c366b2405943bf2b6e3c4f7f358aaddb
Naty T <nathan11002@gmail.com>
2021-02-16 04:25:55 +0300

    wrap up

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -102,6 +102,7 @@ function reset() {
   currentMonsterHealth = chosenMaxLife;
   currentPlayerHealth = chosenMaxLife;
   resetGame(chosenMaxLife);
+  getMaxLifeValues();
 }
 
 function endRound() {
